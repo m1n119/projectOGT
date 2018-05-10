50fa234bfed3c3fd35dca601bcce46c413266758
ogt <m1n119@icloud.com>
2018-05-11 03:07:41 +0900

    カメラシェイク仮実装

--- Test Projecct 2/Assets/TestScene/CameraScript.cs
+++ Test Projecct 2/Assets/TestScene/CameraScript.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraScript : MonoBehaviour {
+
     private Transform target;
-	// Use this for initialization
+    public Vector3 offset = Vector3.zero;
+   	// Use this for initialization
 	void Start () {
         target = GameObject.FindWithTag("Player").transform;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        transform.position = new Vector3(target.position.x, target.transform.position.y, transform.position.z);
+	void Update ()
+    {
+        transform.position = target.transform.position + offset;
     }
+
 }
--- Test Projecct 2/Assets/TestScene/ShakeCamera.cs
+++ Test Projecct 2/Assets/TestScene/ShakeCamera.cs
@@ -4,15 +4,17 @@ using System.Collections;
 public class ShakeCamera : MonoBehaviour
 {
     public float setShakeTIme; // 持続振動時間
-
     private float lifeTime;
     private Vector3 savePosition;
+    private Vector3 offset;
     private float lowRangeX;
     private float maxRangeX;
     private float lowRangeY;
     private float maxRangeY;
+    private float Zjiku;
     static public bool ShakeFlg = false;
     public bool GetShakeFlg() { return ShakeFlg; }
+    private float timer;
 
     void Start()
     {
@@ -20,27 +22,36 @@ public class ShakeCamera : MonoBehaviour
             setShakeTIme = 0.2f;
         lifeTime = 0.0f;
         ShakeFlg = false;
+        offset = GetComponent<CameraScript>().offset;
     }
 
     void Update()
     {
-        if (lifeTime < 0.0f)
+        if (lifeTime <= 0.0f)
         {
-            transform.position = savePosition;
+            timer = 0;
+            offset.z = -1;
+            GetComponent<CameraScript>().offset = offset;
             lifeTime = 0.0f;
             ShakeFlg = false;
         }
 
         if (lifeTime > 0.0f)
         {
+            timer++;
             ShakeFlg = true;
             lifeTime -= Time.deltaTime;
             float x_val = Random.Range(lowRangeX, maxRangeX);
             float y_val = Random.Range(lowRangeY, maxRangeY);
-            transform.position = new Vector3(x_val, y_val, transform.position.z);
+            //transform.position = new Vector3(x_val, y_val, transform.position.z);
+            GetComponent<CameraScript>().offset = new Vector3(x_val, y_val, -1);
         }
 
-        if (Input.GetButtonDown("Jump"))
+        if (timer > 60)
+        {
+            lifeTime = 0.0f;
+        }
+        if (Input.GetKeyDown("space"))
         {
             if (ShakeFlg == false) CatchShake();
         }
@@ -48,11 +59,12 @@ public class ShakeCamera : MonoBehaviour
 
     public void CatchShake()
     {
-        savePosition = transform.position;
-        lowRangeY = savePosition.y - 1.0f;
-        maxRangeY = savePosition.y + 1.0f;
-        lowRangeX = savePosition.x - 1.0f;
-        maxRangeX = savePosition.x + 1.0f;
+        if (ShakeFlg == true) return;
+        savePosition = offset;
+        lowRangeY = savePosition.y - 0.1f;
+        maxRangeY = savePosition.y + 0.1f;
+        lowRangeX = savePosition.x - 0.1f;
+        maxRangeX = savePosition.x + 0.1f;
         lifeTime = setShakeTIme;
     }
 }
