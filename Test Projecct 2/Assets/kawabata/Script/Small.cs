1ad92ca929254a3f3cf51822177ae21a52874408
ogt <m1n119@icloud.com>
2018-05-14 13:16:18 +0900

    KWBT 統合バージョン

--- Test Projecct 2/Assets/kawabata/Script/Small.cs
+++ Test Projecct 2/Assets/kawabata/Script/Small.cs
@@ -14,6 +14,7 @@ public class Small : MonoBehaviour
     public GameObject FireEffect;
     public GameObject Fire;
     public GameObject PlusEffect;
+    public GameObject IrandFireEffect;
 
 
     private CircleCollider2D Cc2d;
@@ -22,12 +23,14 @@ public class Small : MonoBehaviour
     public float 温度加算数;
 
     float HitStop;
+    public bool CameraZoom;
     // Use this for initialization
     void Start()
     {
         SmallFire = false;
         Cc2d = GetComponent<CircleCollider2D>();
         TemperatureCheck = GameObject.Find("Slider");
+        CameraZoom = false;
         HitStop = 0;
     }
 
@@ -41,9 +44,14 @@ public class Small : MonoBehaviour
             changeBlue = 0.0f;
             Cc2d.radius = 4.0f;
         }
-        if (Time.timeScale == 0.0f) HitStop++;
+        if (Time.timeScale <= 0.0f)
+        {
+            HitStop++;
+            CameraZoom = true;
+        }
         else HitStop = 0;
-        if (HitStop > 3)
+        if (HitStop > 8) CameraZoom = false;
+        if (HitStop > 10)
         {
             Time.timeScale = 1.0f;
             HitStop = 0;
@@ -62,11 +70,12 @@ public class Small : MonoBehaviour
 
             GameObject ef = Instantiate(PlusEffect, transform.position, Quaternion.identity) as GameObject;
             Destroy(ef, 0.37f);
+            GameObject ef5 = Instantiate(IrandFireEffect, transform.position, Quaternion.identity) as GameObject;
+            Destroy(ef5, 0.3f);
             GameObject ef2 = Instantiate(FireEffect, transform.position, Quaternion.identity) as GameObject;
             GameObject ef3 = Instantiate(Fire, transform.position, Quaternion.identity) as GameObject;
 
             SmallFire = true;
-
             // 2018/05/10 00:37 追加(OGT)
             LogManager.AddLog(this.name + "が燃えました");
         }
